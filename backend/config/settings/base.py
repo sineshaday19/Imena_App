e52cb4f631eba59f2ef109b2756ecca7d41ad675
sineshaday19 <sineshaday19@gmail.com>
2026-03-03 01:39:16 +0100

    Add gunicorn and whitenoise for Render deployment

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
